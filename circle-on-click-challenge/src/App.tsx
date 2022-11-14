8d3467d5e58277e9ca58ee8b7887de99df3d7b95
MrWillian <willian.dev10@gmail.com>
2022-11-14 15:34:55 -0300

    refactor: implement redo and disable buttons

--- circle-on-click-challenge/src/App.tsx
+++ circle-on-click-challenge/src/App.tsx
@@ -1,14 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 type Point = { x: number, y: number }
 
 function App() {
   const [ points, setPoints ] = useState<Point[]>([]);
-
-  useEffect(() => {
-    console.log(points);
-  }, [points]);
+  const [ removedPoints, setRemovedPoints] = useState<Point[]>([]);
 
   const handleNewClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const { clientX: pointX, clientY: pointY } = e;
@@ -17,35 +14,29 @@ function App() {
 
   const handleUndoCircle = () => {
     const pointToRemove = points.pop();
+    setRemovedPoints([...removedPoints, pointToRemove]);
     setPoints(points.filter(point => point !== pointToRemove));
   }
 
-  const handleRedoCircle = () => {}
+  const handleRedoCircle = () => {
+    setPoints([...points, removedPoints[removedPoints.length - 1]]);
+    setRemovedPoints(removedPoints.filter((_, index) => index !== removedPoints.length - 1));
+  }
 
   return (
     <>
-      <button onClick={handleUndoCircle} className="action-buttons">Undo</button>
-      <button onClick={handleRedoCircle} className="action-buttons">Redo</button>
+      <button onClick={handleUndoCircle} className="action-buttons" disabled={points.length < 1 ? true : false}>Undo</button>
+      <button onClick={handleRedoCircle} className="action-buttons" disabled={removedPoints.length < 1 ? true : false}>Redo</button>
       <div className="App" onClick={handleNewClick}>
         {points.map((point, index) => (
-          <>
-            <div 
-              key={index} 
-              className="circle" 
-              style={{
-                left: point.x - 10 + "px", 
-                top: point.y - 10 + "px",
-              }} 
-            />
-            <span className="labels" style={{
-                left: point.x - 10 + "px", 
-                top: point.y - 10 + "px",
-              }} >{point.x}</span>
-            <span className="labels" style={{
-                left: point.x + "px", 
-                top: point.y + "px",
-              }} >{point.y}</span>
-          </>
+          <div 
+            key={index} 
+            className="circle" 
+            style={{
+              left: point.x - 10 + "px", 
+              top: point.y - 10 + "px",
+            }} 
+          />
         ))}
       </div>
     </>
