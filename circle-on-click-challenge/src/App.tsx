a4a104f8dbcca999408ea28d5a6a4ef1dd3c65e5
MrWillian <willian.dev10@gmail.com>
2022-11-14 11:30:07 -0300

    refactor: increase newClick to place circles on the screen

---
+++ circle-on-click-challenge/src/App.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+type Point = { x: number, y: number }
+
+function App() {
+  const [ points, setPoints ] = useState<Point[]>([]);
+
+  useEffect(() => {
+    console.log(points);
+  }, [points]);
+
+  const handleNewClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { clientX: pointX, clientY: pointY } = e;
+    setPoints([...points, { x: pointX, y: pointY }]);
+  }
+
+  const handleUndoCircle = () => {
+    const newPoints = points.pop();
+    setPoints([...points, points.pop()]);
+  }
+
+  const handleRedoCircle = () => {}
+
+  return (
+    <>
+      <button onClick={handleUndoCircle} className="action-buttons">Undo</button>
+      <button onClick={handleRedoCircle} className="action-buttons">Redo</button>
+      <div className="App" onClick={handleNewClick}>
+        {points.map((point, index) => (
+          <>
+            <div 
+              key={index} 
+              className="circle" 
+              style={{
+                left: point.x - 10 + "px", 
+                top: point.y - 10 + "px",
+              }} 
+            />
+            <span className="labels" style={{
+                left: point.x - 10 + "px", 
+                top: point.y - 10 + "px",
+              }} >{point.x}</span>
+            <span className="labels" style={{
+                left: point.x + "px", 
+                top: point.y + "px",
+              }} >{point.y}</span>
+          </>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
