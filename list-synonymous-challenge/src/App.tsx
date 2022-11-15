51195f6ffbc47d8b7dfa3a9ccb70a9197decb0cc
MrWillian <willian.dev10@gmail.com>
2022-11-15 09:37:58 -0300

    refactor: getting synonymous by word

--- list-synonymous-challenge/src/App.tsx
+++ list-synonymous-challenge/src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [ word, setWord ] = useState<string>('');
+  const [ synonymous, setSynonymous ] = useState<string[]>([]);
+
+  const getSynonymous = () => {
+    axios.get(`https://api.datamuse.com/words?rel_syn=${word}`)
+      .then(response => console.log('response', response.data))
+      .catch(err => console.log('error', err));
+  }
+
+  const handleSubmitFormAction = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    getSynonymous();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <form className="App-form">
-          <input type="text" className="App-form-input" />
-          <button type="submit" className="App-form-button">Enviar</button>
+          <input 
+            type="text" 
+            className="App-form-input" 
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <button type="submit" className="App-form-button" onClick={(e) => handleSubmitFormAction(e)}>Enviar</button>
         </form>
         <ul className="App-list">
           <li className="App-list-item">teste 1</li>
