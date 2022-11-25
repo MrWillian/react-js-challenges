d172dbdd3080c306f8ff548e931b931288a2c013
MrWillian <willian.dev10@gmail.com>
2022-11-25 15:31:25 -0300

    refactor(react-query): implement react-query to fetching data

--- list-synonymous-challenge/src/App.tsx
+++ list-synonymous-challenge/src/App.tsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
 function App() {
   const [ word, setWord ] = useState<string>('');
-  const [ synonymous, setSynonymous ] = useState<string[]>([]);
+  const { data, refetch } = useQuery(["synonymous"], () => getSynonymous(), { 
+    refetchOnWindowFocus: false,
+    enabled: false
+  });
 
-  const getSynonymous = (wordToSearch: string) => {
-    axios.get(`https://api.datamuse.com/words?rel_syn=${wordToSearch}`)
+  useEffect(() => {
+    setTimeout(() => {
+      refetch();
+    }, 500);
+  }, [word]);
+
+  const getSynonymous = async () => {
+    return await axios.get(`https://api.datamuse.com/words?rel_syn=${word}`)
       .then(response => response.data)
       .then(data => data.map((item: any) => item.word))
-      .then(item => setSynonymous(item))
       .catch(err => console.log('error', err));
   }
 
-  const handleSubmitFormAction = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    getSynonymous(word);
-  }
+  const handleSubmitFormAction = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => e.preventDefault();
 
-  const handleListItemClickAction = (e: React.MouseEvent<HTMLLIElement, MouseEvent>, element: string) => {
-    setWord(element);
-    getSynonymous(element);
-  }
+  const handleListItemClickAction = (e: React.MouseEvent<HTMLLIElement, MouseEvent>, element: string) => setWord(element);
 
   return (
     <div className="App">
@@ -37,7 +40,7 @@ function App() {
           <button type="submit" className="App-form-button" onClick={(e) => handleSubmitFormAction(e)}>Enviar</button>
         </form>
         <ul className="App-list">
-          {synonymous.length > 0 ? synonymous.map((element: string, index: number) => 
+          {data?.length > 0 ? data?.map((element: string, index: number) => 
             <li className="App-list-item" key={index} onClick={(e => handleListItemClickAction(e, element))}>{element}</li>
           ) : <>Nenhuma correspondência...</>}
         </ul>
